8f3b852bac0b554f8665cc9cd52dc130bc91ef60
kasfandiyarov <kasfandiyarov@gmail.com>
2019-09-13 13:59:23 +0300

    update gradle and build script

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,13 @@
 
 object Vers {
+    //Plugins
+    const val dokkav = "0.9.18"
+    const val gradle_release_plugin = "1.3.9"
+    const val asciidoctor = "1.5.9.2"
+
+    //Dependencies
     val kotlin = "1.3.41"
     val dokka = "0.9.18"
-    val gradle_release_plugin = "1.3.8"
     val jfix_stdlib = "1.0.22"
     val lombok = "1.18.4"
     val jackson = "2.9.6"
@@ -14,12 +19,17 @@ object Vers {
 }
 
 object Libs {
+    //Plugins
+    const val gradle_release_plugin = "ru.fix:gradle-release-plugin:${Vers.gradle_release_plugin}"
+    const val dokka_gradle_plugin =  "org.jetbrains.dokka:dokka-gradle-plugin:${Vers.dokkav}"
+    const val asciidoctor = "org.asciidoctor:asciidoctor-gradle-plugin:${Vers.asciidoctor}"
+    const val nexus_staging_plugin = "io.codearte.nexus-staging"
+    const val nexus_publish_plugin = "de.marcphilipp.nexus-publish"
+
+    //Dependencies
     val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Vers.kotlin}"
     val kotlin_jdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Vers.kotlin}"
     val kotlin_reflect = "org.jetbrains.kotlin:kotlin-reflect:${Vers.kotlin}"
-
-    val gradle_release_plugin = "ru.fix:gradle-release-plugin:${Vers.gradle_release_plugin}"
-    val dokka_gradle_plugin = "org.jetbrains.dokka:dokka-gradle-plugin:${Vers.dokka}"
 
     val jfix_stdlib_socket = "ru.fix:jfix-stdlib-socket:${Vers.jfix_stdlib}"
     val lombok = "org.projectlombok:lombok:${Vers.lombok}"

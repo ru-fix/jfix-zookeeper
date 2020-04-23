115665433449b937c41a3b5778c9b7daf79ca719
Azat Mukhametzyanov <phoenigm@ya.ru>
2020-04-23 16:13:16 +0300

    refactor lock test

--- jfix-zookeeper/src/test/kotlin/ru/fix/zookeeper/lock/LockManagerImplTest.kt
+++ jfix-zookeeper/src/test/kotlin/ru/fix/zookeeper/lock/LockManagerImplTest.kt
@@ -1,31 +1,15 @@
 package ru.fix.zookeeper.lock
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import ru.fix.aggregating.profiler.NoopProfiler
-import ru.fix.zookeeper.testing.ZKTestingServer
-import ru.fix.zookeeper.utils.ZkTreePrinter
+import ru.fix.zookeeper.AbstractZookeeperTest
 
-class LockManagerImplTest {
-    private lateinit var zkTestingServer: ZKTestingServer
-    private lateinit var printer: ZkTreePrinter
-    private lateinit var lockManager: LockManager
-
-    companion object {
-        private const val rootPath = "/zk-cluster/wwp"
-    }
-
-    @BeforeEach
-    fun setUp() {
-        zkTestingServer = ZKTestingServer()
-        printer = ZkTreePrinter(zkTestingServer.client)
-        zkTestingServer.start()
-        lockManager = LockManagerImpl(zkTestingServer.client, "worker", NoopProfiler())
-    }
-
+class LockManagerImplTest : AbstractZookeeperTest() {
 
     @Test
     fun test() {
+        val lockManager = LockManagerImpl(testingServer.client, "worker", NoopProfiler())
         lockManager.tryAcquire(LockIdentity("pain", "/path/pain")) { println("fallback") }
     }
+
 }

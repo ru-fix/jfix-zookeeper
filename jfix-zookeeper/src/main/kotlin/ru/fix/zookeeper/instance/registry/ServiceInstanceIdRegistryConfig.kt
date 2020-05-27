d9e7c98a789dd4d173e08e645d61a0056a67c9c8
Azat Mukhametzyanov <amukhametzyanov@fix.ru>
2020-05-27 12:39:12 +0300

    add javadocs

--- jfix-zookeeper/src/main/kotlin/ru/fix/zookeeper/instance/registry/ServiceInstanceIdRegistryConfig.kt
+++ jfix-zookeeper/src/main/kotlin/ru/fix/zookeeper/instance/registry/ServiceInstanceIdRegistryConfig.kt
@@ -7,7 +7,15 @@ class ServiceInstanceIdRegistryConfig(
         val serviceName: String,
         val countRegistrationAttempts: Int = 20,
         val serviceRegistrationPath: String = "$rootPath/services",
-        val disconnectTimeout: Duration = Duration.ofSeconds(10),
+        /**
+         * Pair host + port as application's primary key.
+         */
         val host: String,
-        val port: Int
+        val port: Int,
+        /**
+         * Timeout during which the client does not connect with the same host and port.
+         * After this timeout expiration, new client can get instance id from zk,
+         * if host and port of connected client and host and port of instance in zk are equal.
+         */
+        val disconnectTimeout: Duration = Duration.ofSeconds(30)
 )
--- jfix-zookeeper/src/main/kotlin/ru/fix/zookeeper/lock/PersistentExpiringLockManagerConfig.kt
+++ jfix-zookeeper/src/main/kotlin/ru/fix/zookeeper/lock/PersistentExpiringLockManagerConfig.kt
@@ -3,8 +3,22 @@ package ru.fix.zookeeper.lock
 import java.time.Duration
 
 data class PersistentExpiringLockManagerConfig(
-        val reservationPeriod: Duration = Duration.ofSeconds(3),
-        val acquiringTimeout: Duration = Duration.ofSeconds(3),
-        val expirationPeriod: Duration = Duration.ofSeconds(0),
-        val lockProlongationInterval: Duration = Duration.ofSeconds(2)
+        /**
+         *  How long to hold lock
+         */
+        val reservationPeriod: Duration = Duration.ofSeconds(60),
+        /**
+         * how long lock acquiring can be performed
+         */
+        val acquiringTimeout: Duration = Duration.ofSeconds(2),
+        /**
+         * time for checking lock expiration.
+         * If lock expires after lock.expirationTime + expiration period,
+         * then prolong this lock, else do nothing
+         */
+        val expirationPeriod: Duration = Duration.ofSeconds(20),
+        /**
+         * how often to prolong locks
+         */
+        val lockProlongationInterval: Duration = Duration.ofSeconds(10)
 )
